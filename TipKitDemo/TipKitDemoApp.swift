edbdf9ae6c3fc26ac9d32dd640a3d80e68746e98
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2025-01-18 10:07:41 -0500

    Initial Commit

---
+++ TipKitDemo/TipKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitDemoApp.swift
+//  TipKitDemo
+//
+//  Created by Serhii Lukashchuk on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TipKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
