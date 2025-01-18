2a191e360dc23e413e8cc9a47a631f61a9803884
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2025-01-18 10:16:27 -0500

    add button and state

--- TipKitDemo/ContentView.swift
+++ TipKitDemo/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var imageName = "globe"
+    @State private var textValue: String = "Hello, World!"
+    @State var count = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: imageName)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text(textValue)
+            
+            Button("\(count)") {
+                count += 1
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.largeTitle)
+            .bold()
         }
         .padding()
     }
