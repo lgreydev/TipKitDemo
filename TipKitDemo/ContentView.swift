850d9e07257d7664755853fe33bc794ee2d85e07
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2025-01-21 19:49:49 -0500

    add Image Tip

--- TipKitDemo/ContentView.swift
+++ TipKitDemo/ContentView.swift
@@ -11,6 +11,7 @@ import TipKit
 struct ContentView: View {
     
     private let imageTip = ImageTip()
+    private let textTip = ImageTip()
     
     @State private var imageName = "globe"
     @State private var textValue: String = "Hello, World!"
@@ -37,6 +38,18 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             
             Text(textValue)
+                .popoverTip(textTip, arrowEdge: .top) { action in
+                    switch action.id {
+                    case "change":
+                        textValue = "Hello!"
+                        imageTip.invalidate(reason: .actionPerformed)
+                    case "close":
+                        print("closed")
+                        imageTip.invalidate(reason: .tipClosed)
+                    default:
+                        break
+                    }
+                }
             
             Button("\(count)") {
                 count += 1
@@ -66,7 +79,27 @@ struct ImageTip: Tip {
         [
             Action(id: "change", title: "Go to sleep"),
             Action(id: "close", title: "Close")
-            
+        ]
+    }
+}
+
+struct TextTip: Tip {
+    var title: Text {
+        Text("This is Image view")
+    }
+    
+    var message: Text? {
+        Text("You can change it too")
+    }
+    
+    var image : Image? {
+        Image(systemName: "textformat.alt")
+    }
+    
+    var actions: [Action] {
+        [
+            Action(id: "change", title: "Go to sleep"),
+            Action(id: "close", title: "Close")
         ]
     }
 }
